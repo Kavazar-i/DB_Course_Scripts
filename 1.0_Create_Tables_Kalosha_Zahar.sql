d70ee026a309da01234ddc36114f9d10c82b69b8
Zahar Kalosha <99528436+Kavazar-i@users.noreply.github.com>
2024-06-08 18:06:47 +0000

    Update init script

--- 1.0_Create_Tables_Kalosha_Zahar.sql
+++ 1.0_Create_Tables_Kalosha_Zahar.sql
@@ -1,3 +1,9 @@
+-- -- Создание базы данных DIY_Electronics_Kits
+-- CREATE DATABASE DIY_Electronics_Kits;
+
+-- -- Использование созданной базы данных
+-- \c DIY_Electronics_Kits;
+
 -- Таблица Пользователи
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
@@ -16,7 +22,7 @@ CREATE TABLE UserProfiles (
     last_name VARCHAR(255),
     phone_number VARCHAR(20),
     address VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 -- Таблица Категории
@@ -36,7 +42,7 @@ CREATE TABLE Products (
     category_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id) ON DELETE SET NULL
 );
 
 -- Таблица Заказы
@@ -46,7 +52,7 @@ CREATE TABLE Orders (
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50) NOT NULL,
     total_amount DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 -- Таблица Детали заказов
@@ -56,8 +62,8 @@ CREATE TABLE OrderDetails (
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE
 );
 
 -- Таблица Поставщики
@@ -77,8 +83,8 @@ CREATE TABLE ProductSuppliers (
     supplier_id INT NOT NULL,
     supply_price DECIMAL(10, 2),
     supply_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES Products(product_id),
-    FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id)
+    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id) ON DELETE CASCADE
 );
 
 -- Таблица Отзывы
@@ -89,8 +95,8 @@ CREATE TABLE Reviews (
     rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
     comment TEXT,
     review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES Products(product_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 -- Таблица Списки желаемого
@@ -98,7 +104,7 @@ CREATE TABLE Wishlists (
     wishlist_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 -- Таблица Элементы списков желаемого
@@ -106,6 +112,6 @@ CREATE TABLE WishlistItems (
     wishlist_item_id SERIAL PRIMARY KEY,
     wishlist_id INT NOT NULL,
     product_id INT NOT NULL,
-    FOREIGN KEY (wishlist_id) REFERENCES Wishlists(wishlist_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    FOREIGN KEY (wishlist_id) REFERENCES Wishlists(wishlist_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id) ON DELETE CASCADE
 );
